112bb5dd64f79e6e802d5859901253dc5be0c55e
mccaffertycr <mccaffertycr@gmail.com>
2018-08-15 15:59:36 -0400

    to-do-list files copied, npm installed

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');              
+var bodyParser = require('body-parser');
+var path = require('path');
+           require('dotenv').config();
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+var todoController = require('./controllers/todo_troller');
+
+app.use('/', todoController);
+
+
+app.listen(PORT, () => {
+    console.log('listening on: http://localhost:' + PORT);
+});
