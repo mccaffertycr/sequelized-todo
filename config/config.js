678a08f16a65b67cf71cdbb6fa5bf6cfa03b606f
mccaffertycr <mccaffertycr@gmail.com>
2018-08-18 15:28:16 -0400

    config

--- config/config.js
+++ config/config.js
@@ -15,12 +15,9 @@ module.exports = {
    host: process.env.DB_HOSTNAME,
     dialect: "mysql"
   },
-  production: {
-   username: process.env.DB_USERNAME,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
-   host: process.env.DB_HOSTNAME,
-   dialect: 'mysql'
+  "production": {
+   "use_env_variable": "JAWSDB_URL",
+   "dialect": "mysql"
  }
 };
 
