e36b1fabb4c22cc786fc344f74ccd5f92e97040d
mccaffertycr <mccaffertycr@gmail.com>
2018-09-01 13:53:17 -0400

    updated with loose passport implementation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express');              
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const session  = require('express-session');             
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config();
 const db = require('./models');
 
 const app = express();
@@ -10,6 +12,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// For Passport 
+app.use(session({ secret: process.env.PASSPORT_SEC, resave: true, saveUninitialized: true})); // session secret 
+app.use(passport.initialize()); 
+app.use(passport.session()); // persistent login sessions
 
 
 const exphbs = require('express-handlebars');
